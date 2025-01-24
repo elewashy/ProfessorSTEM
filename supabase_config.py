5e5e96e985ddeb3477b97666edcb84a21c67febf
elewashy <elewashy9@gmail.com>
2025-01-24 15:17:36 +0200

    Update

--- supabase_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from supabase import create_client
-
-# إعداد Supabase Client
-def get_supabase_client():
-    url = os.getenv("SUPABASE_URL")  # رابط المشروع من Supabase
-    key = os.getenv("SUPABASE_KEY")  # مفتاح service_role
-    return create_client(url, key)
