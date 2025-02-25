9c522291997be8be81a679a729fcbaf7c237d3a3
elewashy <elewashy9@gmail.com>
2025-02-25 20:12:38 +0200

    Update

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, session
 from auth import signin, signup
 from db import get_db_connection
+from config_agent import SECRET_KEY
+import routes
 
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
+app.secret_key = SECRET_KEY
 
 @app.route('/')
 def home():
@@ -45,6 +48,13 @@ def page_not_found(e):
 @app.route('/todolist')
 def todolist():
     return render_template('todolist.html')
+
+app.add_url_rule('/agents', view_func=routes.agents_home)
+app.add_url_rule('/start_learning', view_func=routes.start_learning, methods=['POST'])
+app.add_url_rule('/quiz', view_func=routes.quiz)
+app.add_url_rule('/submit_answer', view_func=routes.submit_answer, methods=['POST'])
+app.add_url_rule('/results', view_func=routes.results)
+app.add_url_rule('/study_plan', view_func=routes.study_plan)
 
 if __name__ == "__main__":
     app.run(debug=True)
